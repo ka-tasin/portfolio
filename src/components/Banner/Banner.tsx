c32d73fe8c32b88c49c3b0bb224582122d62e939
Kausar Ahmad Tasin <67546210+ka-tasin@users.noreply.github.com>
2026-01-21 14:17:21 +0600

    Update Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -144,7 +144,7 @@ const Banner = (): React.ReactElement => {
                   <div className="flex items-center gap-2 mb-2">
                     <FiDatabase className="w-5 h-5 text-gray-900" />
                     <span className="text-2xl font-bold text-gray-900">
-                      50+
+                      7+
                     </span>
                   </div>
                   <span className="text-sm text-gray-600">
